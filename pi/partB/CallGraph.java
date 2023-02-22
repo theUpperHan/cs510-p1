28a213f2bfed47fff1bc54a869d1c2456cd12474
theUpperHan <huan1482@purdue.edu>
2023-02-21 20:37:48 -0500

    Expand function calls

--- pi/partB/CallGraph.java
+++ pi/partB/CallGraph.java
@@ -1,12 +1,16 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.lang.StringBuilder;
+import java.util.Map;
+import java.util.HashMap;
 
 public class CallGraph {
     private List<Function> functions;
+    private Map<String, List<String>> functionsMap;
 
     public CallGraph() {
         functions = new ArrayList<>();
+        functionsMap = new HashMap<>();
     }
 
     public List<Function> getFunctions() {
@@ -15,6 +19,7 @@ public class CallGraph {
 
     public void addFunction(Function f) {
         functions.add(f);
+        functionsMap.put(f.getName(), f.getFunctionCalls());
     }
 
     @Override
@@ -28,5 +33,22 @@ public class CallGraph {
         }
         sb.append("}");
         return sb.toString();
+    }
+
+    public void expand() {
+        for (Function func_entry : functions) {
+            List<String> expanded = new ArrayList<>(); //expanded function calls
+            
+            for (String func : func_entry.getFunctionCalls()) {
+                if (functionsMap.get(func).size() > 0) { //if expandable
+                    expanded.addAll(functionsMap.get(func));
+                }
+                else {
+                    expanded.add(func);
+                }
+            }
+
+            func_entry.setFunctionCalls(expanded);
+        }
     }
 }
